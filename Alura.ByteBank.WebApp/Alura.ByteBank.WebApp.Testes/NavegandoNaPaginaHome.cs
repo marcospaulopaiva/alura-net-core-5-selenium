5038c8d7ab8191774aa6b0e3cdd1c08e4981b3d8
Marcos Paulo <marcos.poo@gmail.com>
2024-02-21 21:27:42 -0300

    Aula 2 - Iniciando com Selenium WebDriver.
    Nessa aula, aprendemos:
    - Criar um projeto do xUnit e configurar a biblioteca do Selenium WebDriver para criar um teste automatizado da interface da aplicação;
    - Criar nosso primeiro teste usando o Selenium WebDriver;
    - Recuperar um código gerado pelo Selenium IDE como um teste na nossa suite de testes.

---
+++ Alura.ByteBank.WebApp/Alura.ByteBank.WebApp.Testes/NavegandoNaPaginaHome.cs
@@ -0,0 +1,51 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+using System.Reflection;
+using Xunit;
+
+namespace Alura.ByteBank.WebApp.Testes
+{
+    public class NavegandoNaPaginaHome
+    {
+        [Fact]
+        public void CarregaPaginaHomeEVerificaTituloDaPagina()
+        {
+            //Arrange
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            //Act
+            driver.Navigate().GoToUrl("https://localhost:44309");
+            //Assert
+            Assert.Contains("WebApp", driver.Title);
+        }
+
+        [Fact]
+        public void CarregaPaginaHomeVerificaExistenciaLinkLoginEHome()
+        {
+            //Arrange
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            //Act
+            driver.Navigate().GoToUrl("https://localhost:44309");
+            //Assert
+            Assert.Contains("Login", driver.PageSource);
+            Assert.Contains("Home", driver.PageSource);
+        }
+
+        [Fact]
+        public void LogandoNoSistema()
+        {
+            //Arrange
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            driver.Navigate().GoToUrl("https://localhost:44309/");
+            driver.Manage().Window.Size = new System.Drawing.Size(1294, 869);
+            driver.FindElement(By.LinkText("Login")).Click();
+            driver.FindElement(By.Id("Email")).SendKeys("andre@email.com");
+            driver.FindElement(By.Id("Senha")).SendKeys("senha01");
+            driver.FindElement(By.Id("btn-logar")).Click();
+            driver.FindElement(By.CssSelector(".btn")).Click();
+            driver.FindElement(By.Id("Email")).SendKeys("andre@email.com");
+            driver.FindElement(By.Id("Senha")).SendKeys("senha01");
+        }
+
+    }
+}
