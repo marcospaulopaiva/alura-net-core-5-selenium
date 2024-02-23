5dee734ee421018eb1ba1f34aa11b8e463825f41
Marcos Paulo <marcos.poo@gmail.com>
2024-02-22 21:42:39 -0300

    Aula 4 - Testando com localizadores.
    Nessa aula, aprendemos:
    - Outras formas de capturar os elementos de uma página HTML para não ficarmos restritos ao Id ou Name;
    - Criar uma lista de elementos que estão na página usando a biblioteca do Selenium WebDriver, o que pode deixar nosso código mais dinâmico;
    - Escrever no console do gerenciador de testes usando recursos do próprio xUnit.

--- Alura.ByteBank.WebApp/Alura.ByteBank.WebApp.Testes/AposRealizarLogin.cs
+++ Alura.ByteBank.WebApp/Alura.ByteBank.WebApp.Testes/AposRealizarLogin.cs
@@ -1,19 +1,24 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Alura.ByteBank.WebApp.Testes
 {
     public class AposRealizarLogin
     {
         private IWebDriver _driver;
+        private ITestOutputHelper _saidaConsoleTeste;
 
-        public AposRealizarLogin()
+        public AposRealizarLogin(ITestOutputHelper saidaConsoleTeste)
         {
             _driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
             _driver.Navigate().GoToUrl("https://localhost:44309/UsuarioApps/Login");
+            _saidaConsoleTeste = saidaConsoleTeste;
         }
 
         [Fact]
@@ -67,6 +72,69 @@ namespace Alura.ByteBank.WebApp.Testes
             //Assert
             Assert.Contains("Login", _driver.PageSource);
         }
+
+        [Fact]
+        public void RealizarLoginAcessaMenuECadastraCliente()
+        {
+            //Arrange
+            var login = _driver.FindElement(By.Id("Email"));
+            var senha = _driver.FindElement(By.Id("Senha"));
+            login.SendKeys("andre@email.com");
+            senha.SendKeys("senha01");
+
+            _driver.FindElement(By.Id("btn-logar")).Click();
+            _driver.FindElement(By.LinkText("Cliente")).Click();
+            _driver.FindElement(By.LinkText("Adicionar Cliente")).Click();
+
+            _driver.FindElement(By.Name("Identificador")).Click();
+            _driver.FindElement(By.Name("Identificador")).SendKeys("20cd1c01-5fbf-40b7-b41b-0341bd38fc32");
+            
+            _driver.FindElement(By.Name("CPF")).Click();
+            _driver.FindElement(By.Name("CPF")).SendKeys("536.186.690-38");
+
+            _driver.FindElement(By.Name("Nome")).Click();
+            _driver.FindElement(By.Name("Nome")).SendKeys("Marcos Paulo");
+
+            _driver.FindElement(By.Name("Profissao")).Click();
+            _driver.FindElement(By.Name("Profissao")).SendKeys("Desenvolvedor");
+
+            //Act
+            _driver.FindElement(By.CssSelector(".btn-primary")).Click();
+            _driver.FindElement(By.LinkText("Home")).Click();
+
+            //Assert
+            Assert.Contains("Logout", _driver.PageSource);
+        }
+
+        [Fact]
+        public void RealizarLoginAcessaListagemDeContas()
+        {
+            //Arrange
+            var login = _driver.FindElement(By.Id("Email"));
+            var senha = _driver.FindElement(By.Id("Senha"));
+            login.SendKeys("andre@email.com");
+            senha.SendKeys("senha01");
+            
+            _driver.FindElement(By.Id("btn-logar")).Click();
+            _driver.FindElement(By.Id("contacorrente")).Click();
+
+            IReadOnlyCollection<IWebElement> elements = _driver.FindElements(By.TagName("a"));
+
+            foreach (IWebElement e in elements) 
+            { 
+                _saidaConsoleTeste.WriteLine(e.Text); 
+            }
+
+            var elemento = (from webElemento in elements
+                            where webElemento.Text.Contains("Detalhes")
+                            select webElemento).First();
+            //Act
+            elemento.Click();
+
+            //Assert
+            Assert.Contains("Voltar", _driver.PageSource);
+        }
+
 
     }
 }
